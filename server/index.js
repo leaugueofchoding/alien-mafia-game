2db07488aeec9c247de0e3d36736dcd27b70b79e
leaugueofchoding <hispkty@gmail.com>
2025-06-24 16:24:02 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -278,7 +278,7 @@ function eliminatePlayer(roomCode, playerId, cause = 'unknown') {
   return false;
 }
 
-aapp.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
